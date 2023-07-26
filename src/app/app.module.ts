4127d9bad3fdd49bc3b1479f735f807b7845b2fa
dariannfuentes <dariannfuentes@gmail.com>
2023-07-26 08:48:42 -0300

    Actualice la version de PrimeNg e implemente el menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,34 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 import { SharedModule } from './shared/shared.module';
-
+import { HomeComponent } from './modules/home/home.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { PerfilComponent } from './modules/perfil/perfil.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { RutinasComponent } from './modules/rutinas/rutinas.component';
+import { CrearrutinasComponent } from './modules/crearrutinas/crearrutinas.component';
+import { EjerciciosComponent } from './modules/ejercicios/ejercicios.component';
+import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    PerfilComponent,
+    RutinasComponent,
+    CrearrutinasComponent,
+    EjerciciosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
     SharedModule,
+    MenubarModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './components/footer/footer.component';
-import { PerfilComponent } from '../modules/perfil/perfil.component';
-import { RutinasComponent } from '../modules/rutinas/rutinas.component';
-import { CrearrutinasComponent } from '../modules/crearrutinas/crearrutinas.component';
-import { EjerciciosComponent } from '../modules/ejercicios/ejercicios.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from '../modules/home/home.component';
 import { MenubarModule } from 'primeng/menubar';
 
 
 @NgModule({
   declarations: [
-    FooterComponent,
-    HomeComponent,
-    PerfilComponent,
-    RutinasComponent,
-    CrearrutinasComponent,
-    EjerciciosComponent,
-    NavbarComponent
+
   ],
   imports: [
     CommonModule,
