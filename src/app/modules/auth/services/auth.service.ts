591b60992d4bcab1f349a93166bebdfe7a23ee7e
Gauna1553 <gabrielgauna04@gmail.com>
2023-08-14 10:25:30 -0300

    aplique para retornar los datos ingresados

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -8,5 +8,7 @@ export class AuthService {
 
   constructor(public auth: AngularFireAuth) {}
 
-  registrarse(nombre:string, contraseña:string) {}
+  registrarse(nombre:string, contraseña:string) {
+    return this.auth.createUserWithEmailAndPassword(nombre,contraseña)
+  }
 }
