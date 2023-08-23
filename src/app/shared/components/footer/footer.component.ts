b3204ee0a897ed59f367ccdcab3a6c2cae712341
NataliRodriguez <brendulis.2005@gmail.com>
2023-08-23 11:10:11 -0300

    se cambio el estilo del footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -16,91 +16,19 @@ export class FooterComponent implements OnInit {
         label: 'Conceptos e Informacion',
         icon: 'pi pi-fw pi-file',
         routerLink:"/conceps",
-        /*items: [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-                items: [
-                    {
-                        label: 'Bookmark',
-                        icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Video',
-                        icon: 'pi pi-fw pi-video'
-                    }
-                ]
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
-            },
-            {
-                separator: true
-            },
-            {
-                label: 'Export',
-                icon: 'pi pi-fw pi-external-link'
-            }
-        ]*/
+    
     },
     {
-        label: 'Ejercicios',
+        label: 'Contactos',
         icon: 'pi pi-fw pi-pencil',
         routerLink:"/ejercicios"
-        /*items: [
-            {
-                label: 'Left',
-                icon: 'pi pi-fw pi-align-left'
-            },
-            {
-                label: 'Right',
-                icon: 'pi pi-fw pi-align-right'
-            },
-            {
-                label: 'Center',
-                icon: 'pi pi-fw pi-align-center'
-            },
-            {
-                label: 'Justify',
-                icon: 'pi pi-fw pi-align-justify'
-            }
-        ]*/
+      
     },
     {
-        label: 'Crear Rutina',
+        label: 'Creadores',
         icon: 'pi pi-fw pi-user',
         routerLink:"/crear"
-        /*items: [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-user-plus'
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-user-minus'
-            },
-            {
-                label: 'Search',
-                icon: 'pi pi-fw pi-users',
-                items: [
-                    {
-                        label: 'Filter',
-                        icon: 'pi pi-fw pi-filter',
-                        items: [
-                            {
-                                label: 'Print',
-                                icon: 'pi pi-fw pi-print'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'pi pi-fw pi-bars',
-                        label: 'List'
-                    }
-                ]
-            }
-        ]*/
+       
     },
     
 
