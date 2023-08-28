2584acf373f852c2aa6590377066a3eb707ebebd
Gauna1553 <gabrielgauna04@gmail.com>
2023-08-28 11:15:52 -0300

    defini las variables y funciones para la base de datos

--- src/app/shared/services/firestore.service.ts
+++ src/app/shared/services/firestore.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore'
+import { Usuario } from 'src/app/model/usuarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
+  private usuariosColletion: AngularFirestoreCollection<Usuario>
+  constructor(private database: AngularFirestore) {
+    this.usuariosColletion = this.database.collection<Usuario>('usuarios');
+  }
 
-  constructor() { }
+  agregarUsuario(usuario: Usuario, id: string){
+    //RESOLVE: promesa resulta -> similar al then
+    //REJECT: promesa rechazada -> similar al catch
+    return new Promise(async(resolve,reject) =>{
+
+      try {
+        usuario.uid = id;
+
+        const resultado = await this.usuariosColletion.doc(id).set(usuario);
+        //muestra el resultado sin problema
+        resolve(resultado);
+      } catch(error) {
+        //en caso de que ocurra un error
+        reject(error)
+      }
+    })
+  }
 }
