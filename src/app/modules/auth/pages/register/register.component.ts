f126525b58030959f43bb02e280c14e1f324fcb8
Gauna1553 <gabrielgauna04@gmail.com>
2023-08-28 02:22:47 -0300

    intento corregir el error de la base de datos al cambiar las declaraciones

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -24,11 +24,11 @@ constructor (public servicioAuth: AuthService) {
   //tomamos nuevos registros y tomamos los resultados
   async registrarse() {       
     const credenciales = {
-      nombre: this.usuarios.nombre,
+      email: this.usuarios.email,
       contrasena: this.usuarios.contrasena,
     }
   
-  const res = await this.servicioAuth.registrarse(credenciales.nombre,credenciales.contrasena).then(res => {
+  const res = await this.servicioAuth.registrarse(credenciales.email,credenciales.contrasena).then(res => {
     alert("se agrego un nuevo usuario con exito")
   })
   .catch(error => alert("Hubo un error la registrarse: (\n"+error));
