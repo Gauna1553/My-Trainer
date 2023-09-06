c82157cd874397b20b9e731cf9b1217f0dd2a410
Gauna1553 <gabrielgauna04@gmail.com>
2023-09-06 08:26:04 -0300

    correcciones en las importaciones del app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,14 @@ import { PerfilComponent } from './modules/perfil/perfil.component';
 import { RutinasComponent } from './modules/rutinas/rutinas.component';
 import { CrearrutinasComponent } from './modules/crearrutinas/crearrutinas.component';
 import { EjerciciosComponent } from './modules/ejercicios/ejercicios.component';
-import { CommonModule } from '@angular/common';
-
 import { ConcepinfComponent } from './modules/concepinf/concepinf.component';
+import { EjerciciosService } from './services/ejercicios.service';
+
+import { CommonModule } from '@angular/common';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from 'src/environment/environment';
 import { FormsModule } from '@angular/forms';
-import { EjerciciosService } from './services/ejercicios.service';
 
 
 @NgModule({
