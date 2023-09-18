d07ce074d932c1a2e850e9292b361a5ff02d826e
dariannfuentes <dariannfuentes@gmail.com>
2023-09-18 09:05:18 -0300

    HICE ANDAR EL POPUP IMPORTANDO EL MODULO DE ANIMACIONES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeModule } from './modules/home/home.module';
 import { PerfilModule } from './modules/perfil/perfil.module';
 import { RutinasModule } from './modules/rutinas/rutinas.module';
 import { ConcepinfModule } from './modules/concepinf/concepinf.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import { ConcepinfModule } from './modules/concepinf/concepinf.module';
     HomeModule,
     PerfilModule,
     RutinasModule,
-    ConcepinfModule
+    ConcepinfModule,
+    BrowserAnimationsModule
   ],exports: [
   ],
 
