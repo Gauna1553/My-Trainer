89ddfbfc2d48af5a26e157397bbf4f8c43b7e30d
dariannfuentes <dariannfuentes@gmail.com>
2023-09-21 18:14:26 -0300

    Borre todo lo que no andaba en el ejercicios.service

--- src/app/services/ejercicios.service.ts
+++ src/app/services/ejercicios.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collectionData, collection, addDoc } from '@angular/fire/firestore';
 import { Ejercicio } from '../model/ejercicios';
 import { Observable } from 'rxjs';
 
@@ -9,15 +8,6 @@ import { Observable } from 'rxjs';
 })
 export class EjerciciosService {
 
-  constructor(private moduloFire : Firestore) { }
-
-  addEjercicio(ejercicio: Ejercicio){
-    const ejercicioRef = collection(this.moduloFire, 'ejercicios');
-    return addDoc(ejercicioRef, ejercicio);
-  }
-
-  getEjercicios(): Observable<Ejercicio[]>{
-    const ejercicioRef = collection(this.moduloFire, 'ejercicios');
-      return collectionData(ejercicioRef, { idField: 'id'}) as Observable<Ejercicio[]>;
-  }
+  constructor() { }
+  
 }
