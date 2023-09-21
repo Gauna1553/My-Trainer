2b6a962d4bbecd5dcc1c06295b7b87691de9a82c
dariannfuentes <dariannfuentes@gmail.com>
2023-09-21 18:20:39 -0300

    Rellene con algunos ejercicios para probar si funciona

--- src/app/services/ejercicios.service.ts
+++ src/app/services/ejercicios.service.ts
@@ -12,10 +12,30 @@ export class EjerciciosService {
   getEjercicios(){
     return [
       {
-        nombre: '',
-        grupomuscular: '',
-        rangorep: ''
-      }
+        nombre: 'Press de Banca Plana',
+        grupomuscular: 'Pecho',
+        rangorep: '6-10'
+      },
+      {
+        nombre: 'Press de Banca Inclinada',
+        grupomuscular: 'Pecho',
+        rangorep: '6-10'
+      },
+      {
+        nombre: 'Pec Deck',
+        grupomuscular: 'Pecho',
+        rangorep: '8-12'
+      },
+      {
+        nombre: 'Jalon en polea',
+        grupomuscular: 'Espalda',
+        rangorep: '6-10'
+      },
+      {
+        nombre: 'Remo Gironda',
+        grupomuscular: 'Espalda',
+        rangorep: '6-10'
+      },
     ]
   }
 }
