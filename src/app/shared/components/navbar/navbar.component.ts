52c08ebe0a6b184429c5ec7d0f073d1e4c74527e
dariannfuentes <dariannfuentes@gmail.com>
2023-09-21 16:29:03 -0300

    Acomode el codigo de la navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -15,91 +15,19 @@ export class NavbarComponent implements OnInit{
               label: 'Inicio',
               icon: 'pi pi-fw pi-file',
               routerLink:"/",
-              /*items: [
-                  {
-                      label: 'New',
-                      icon: 'pi pi-fw pi-plus',
-                      items: [
-                          {
-                              label: 'Bookmark',
-                              icon: 'pi pi-fw pi-bookmark'
-                          },
-                          {
-                              label: 'Video',
-                              icon: 'pi pi-fw pi-video'
-                          }
-                      ]
-                  },
-                  {
-                      label: 'Delete',
-                      icon: 'pi pi-fw pi-trash'
-                  },
-                  {
-                      separator: true
-                  },
-                  {
-                      label: 'Export',
-                      icon: 'pi pi-fw pi-external-link'
-                  }
-              ]*/
+              //items: []
           },
           {
               label: 'Ejercicios',
               icon: 'pi pi-fw pi-pencil',
               routerLink:"/ejercicios"
-              /*items: [
-                  {
-                      label: 'Left',
-                      icon: 'pi pi-fw pi-align-left'
-                  },
-                  {
-                      label: 'Right',
-                      icon: 'pi pi-fw pi-align-right'
-                  },
-                  {
-                      label: 'Center',
-                      icon: 'pi pi-fw pi-align-center'
-                  },
-                  {
-                      label: 'Justify',
-                      icon: 'pi pi-fw pi-align-justify'
-                  }
-              ]*/
+              //items: []
           },
           /*{
               label: 'Crear Rutina',
               icon: 'pi pi-fw pi-user',
-              routerLink:"/crear"
-              /*items: [
-                  {
-                      label: 'New',
-                      icon: 'pi pi-fw pi-user-plus'
-                  },
-                  {
-                      label: 'Delete',
-                      icon: 'pi pi-fw pi-user-minus'
-                  },
-                  {
-                      label: 'Search',
-                      icon: 'pi pi-fw pi-users',
-                      items: [
-                          {
-                              label: 'Filter',
-                              icon: 'pi pi-fw pi-filter',
-                              items: [
-                                  {
-                                      label: 'Print',
-                                      icon: 'pi pi-fw pi-print'
-                                  }
-                              ]
-                          },
-                          {
-                              icon: 'pi pi-fw pi-bars',
-                              label: 'List'
-                          }
-                      ]
-                  }
-              ]
+              routerLink:"/crear",
+              //items: []
           },*/
           {
               label: 'Rutinas',
