662bdf8c9b61ad92fe2f450c5477142f1645fe2f
dariannfuentes <dariannfuentes@gmail.com>
2023-09-24 16:37:58 -0300

    Importe los modulos que voy a usar para el rutinas.html

--- src/app/modules/rutinas/rutinas.module.ts
+++ src/app/modules/rutinas/rutinas.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RutinasComponent } from './rutinas.component';
 import { RutinasRoutingModule } from './rutinas-routing.module';
-
-
+import { FieldsetModule } from 'primeng/fieldset';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,9 @@ import { RutinasRoutingModule } from './rutinas-routing.module';
   ],
   imports: [
     CommonModule,
-    RutinasRoutingModule
+    RutinasRoutingModule,
+    FieldsetModule,
+    TableModule
   ]
 })
 export class RutinasModule { }
