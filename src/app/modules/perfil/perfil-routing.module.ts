b861ac6f9705aacda4ad693cce234694515fe929
dariannfuentes <dariannfuentes@gmail.com>
2023-09-24 14:45:59 -0300

    Cree el perfil-routing y configure la conexion con el componente y el modulo

---
+++ src/app/modules/perfil/perfil-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil.component';
+
+
+const routes: Routes = [
+    {path: 'perfil', component: PerfilComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PerfilRoutingModule { }
