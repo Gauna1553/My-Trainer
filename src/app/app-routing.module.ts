4cf7aee012dd77fe210283df1cc58b1a3f9b9ff1
dariannfuentes <dariannfuentes@gmail.com>
2023-09-24 15:04:10 -0300

    Aplique el lazyloading a la ruta de rutinas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   loadChildren:()=>import('./modules/auth/auth.module').then(m =>m.AuthModule)},{
   path: '', component: HomeComponent},{   //ruta de home
   path: '', loadChildren:()=>import('./modules/perfil/perfil.module').then(m=>m.PerfilModule)},{   //ruta de perfil
-  path: 'rutinas', component: RutinasComponent},{   //ruta de rutinas
+  path: '', loadChildren:()=>import('./modules/rutinas/rutinas.module').then(m=>m.RutinasModule)},{   //ruta de rutinas
   path: '', loadChildren:()=>import('./modules/ejercicios/ejercicios.module').then(m=>m.EjerciciosModule)},{   //ruta de ejercicios
   //path: 'crear', component: CrearrutinasComponent},{   //ruta de crearrutinas
   path: 'conceps', component:ConcepinfComponent},
