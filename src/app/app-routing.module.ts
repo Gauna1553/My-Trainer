b63ce753411f1e94692a7b3a7382e1c48d864d86
dariannfuentes <dariannfuentes@gmail.com>
2023-09-24 14:55:07 -0300

    Aplique el lazyloading en la ruta de perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: "auth",
   loadChildren:()=>import('./modules/auth/auth.module').then(m =>m.AuthModule)},{
   path: '', component: HomeComponent},{   //ruta de home
-  path: 'perfil', component: PerfilComponent},{   //ruta de perfil
+  path: '', loadChildren:()=>import('./modules/perfil/perfil.module').then(m=>m.PerfilModule)},{   //ruta de perfil
   path: 'rutinas', component: RutinasComponent},{   //ruta de rutinas
   path: 'ejercicios', component: EjerciciosComponent},{   //ruta de ejercicios
   //path: 'crear', component: CrearrutinasComponent},{   //ruta de crearrutinas
