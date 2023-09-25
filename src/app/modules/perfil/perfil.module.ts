268c6515e3a41daa169b862d814f5fbbb37e319e
dariannfuentes <dariannfuentes@gmail.com>
2023-09-25 08:37:12 -0300

    Implemente el boton de Iniciar Sesion en perfil

--- src/app/modules/perfil/perfil.module.ts
+++ src/app/modules/perfil/perfil.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PerfilComponent } from './perfil.component';
 import { PerfilRoutingModule } from './perfil-routing.module';
+import { ButtonModule } from 'primeng/button';
 
 
 
@@ -11,7 +12,8 @@ import { PerfilRoutingModule } from './perfil-routing.module';
   ],
   imports: [
     CommonModule,
-    PerfilRoutingModule
+    PerfilRoutingModule,
+    ButtonModule
   ]
 })
 export class PerfilModule { }
