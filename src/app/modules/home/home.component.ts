ce8d265094f2d55a14c20e2d4e4002b33b82a06a
Santino0912 <ssantinomunnoz@gmail.com>
2023-09-24 22:46:08 -0300

    agregue la configuracion del carrusel

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -9,5 +9,37 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  images = [
+    {
 
+        imageSrc: "../../../assets/autito1.jpg",
+        thumbnailSrc: "../../../assets/autito1.jpg",
+        alt: "Description for Image 1",
+        title: "Title 1"
+    },
+    {
+      imageSrc: "../../../assets/hondaek9.jpg",
+        thumbnailSrc: "../../../assets/hondaek9.jpg",
+        alt: "Description for Image 1",
+        title: "Title 1"
+
+    },
+    {
+    imageSrc: "../../../assets/audiquattro.jpg",
+        thumbnailSrc: "../../../assets/audiquattro.jpg",
+        alt: "Description for Image 1",
+        title: "Title 1"
+    },
+    {
+      imageSrc: "../../../assets/mustf.webp",
+          thumbnailSrc: "../../../assets/mustf.webp",
+          alt: "Description for Image 1",
+          title: "Title 1"
+      },
+      {
+        imageSrc: "../../../assets/ddsrt.jpg",
+            thumbnailSrc: "../../../assets/ddsrt.jpg",
+            alt: "Description for Image 1",
+            title: "Title 1"
+        },
 }
