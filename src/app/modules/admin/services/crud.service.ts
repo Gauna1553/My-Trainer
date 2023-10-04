4aca0e8357cfc12808547701618df0862c50ee1d
Gauna1553 <gabrielgauna04@gmail.com>
2023-10-04 08:24:03 -0300

    importe y declare que se utilizara en el crud

--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
+import { Ejercicio } from 'src/app/model/ejercicios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
-
-  constructor() { }
+  private ejerciciosColeccion: AngularFirestoreCollection<Ejercicio>
+  constructor(private database: AngularFirestore) {
+    this.ejerciciosColeccion: database.collection('ejecicios')
+  }
 }
