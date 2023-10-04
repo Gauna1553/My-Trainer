cc055398973ba1063178d62ead07f496fb1c1437
Gauna1553 <gabrielgauna04@gmail.com>
2023-10-04 08:27:09 -0300

    declare e importe los modulos a utilizar en el admin module

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Vsitas
+import { AdminRoutingModule } from './admin-routing.module';
+
+//Angular
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ], 
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class AdminModule { }
