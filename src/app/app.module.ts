96565d0001cf4d402023f1d621220386b4717ac8
Gauna1553 <gabrielgauna04@gmail.com>
2023-10-16 15:41:23 -0300

    intente crear las vistas pero no me salio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,10 +22,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 //Los servicios creados
 import { EjerciciosService } from './services/ejercicios.service';
+import { RutinasComponent } from './modules/visitante/components/rutinas/rutinas.component';
+import { HomeComponent } from './modules/visitante/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RutinasComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
