bf271709926d4cad9837c8ae7a4af241e8ec51e3
Gauna1553 <gabrielgauna04@gmail.com>
2023-10-18 09:03:11 -0300

    intente crear los roles de usuario, pero no me salio

--- src/app/modules/roles/rol-usuario.directive.ts
+++ src/app/modules/roles/rol-usuario.directive.ts
@@ -1,10 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Usuario } from 'src/app/model/usuarios';
 
 @Directive({
   selector: '[appRolUsuario]'
 })
 export class RolUsuarioDirective {
+  //private currentUser: RolUsuarioDirective;
+  private permissions = [];
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+    //private user: Usuario
+    ) {
+
+  }
 
 }
