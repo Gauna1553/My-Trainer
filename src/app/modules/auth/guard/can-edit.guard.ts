1fffdd30b8d9b88a26b6c55a73c20cf8cba278de
Gauna1553 <gabrielgauna04@gmail.com>
2023-10-27 08:50:45 -0300

    estoy tratando de arreglar los errores para la creación de roles

--- src/app/modules/auth/guard/can-edit.guard.ts
+++ src/app/modules/auth/guard/can-edit.guard.ts
@@ -1,5 +1,24 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
+import { Observable, map, take, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-export const canEditGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+
+export class canEditGuard implements CanActivateFn {
+  constructor(private authSvc: AuthService) {}
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authSvc.usuario$.pipe (
+      take(1)
+      map((usuario) => usuario && this.authSvc.isAdministrador(usuario))
+      tap(canEdit => {
+        if(!canEdit) {
+          window.alert('Acceso denegado. No tienes permiso de administradro')
+        }
+      })
+    )
+    }
+}
