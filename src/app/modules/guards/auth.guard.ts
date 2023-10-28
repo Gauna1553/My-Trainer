f650fc5bede72b34ee7d21c2bc317b9788fa5ea0
Gauna1553 <gabrielgauna04@gmail.com>
2023-10-28 11:35:07 -0300

    corregi las importaciones de mi guard

--- src/app/modules/guards/auth.guard.ts
+++ src/app/modules/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth/services/auth.service';
 import { take, tap } from 'rxjs';
 
