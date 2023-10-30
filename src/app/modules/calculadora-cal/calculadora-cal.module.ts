3fc06d640885e07f7402664eaec822e53939a7e8
Santino0912 <ssantinomunnoz@gmail.com>
2023-10-30 09:00:59 -0300

    realice todas las importaciones necesarias

--- src/app/modules/calculadora-cal/calculadora-cal.module.ts
+++ src/app/modules/calculadora-cal/calculadora-cal.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraCALComponent } from './calculadora-cal.component';
 import { CalculadoraRoutingModule } from './calculadora-routing.module';
+//Importaciones de Prime
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
 
 
 
@@ -11,7 +20,15 @@ import { CalculadoraRoutingModule } from './calculadora-routing.module';
     ],
   imports: [
     CommonModule,
-    CalculadoraRoutingModule
+    CalculadoraRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DialogModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TriStateCheckboxModule,
+    ProgressSpinnerModule
   ]
 })
 export class CalculadoraCALModule { }
