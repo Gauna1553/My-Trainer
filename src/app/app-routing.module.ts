9e1ae202271b5840e277154fcb4033f8acc1f770
Gauna1553 <gabrielgauna04@gmail.com>
2023-10-30 10:26:22 -0300

    cree una función en el guard para poder asignarle los roles al usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { RoleGuard } from './modules/guard/roles.guard';
+import { Roles } from './model/roles';
 
-import { authGuard } from './modules/guards/auth.guard';
 
 const routes: Routes = [
    //lazyloading
-  {path: "", canActivate:[authGuard],
-  loadChildren:()=>import('./modules/auth/auth.module').then(m =>m.AuthModule)},
-  {path: 'inicio', component: HomeComponent},{   //ruta de home
+  {path: "",loadChildren:()=>import('./modules/auth/auth.module').then(m =>m.AuthModule)},
+  {path: 'inicio', component: HomeComponent,canActivate:[RoleGuard], data: { expect: Roles.Usuario }},{   //ruta de home
   // SE DEJAN ESTAS RUTAS VACIAS PORQUE YA SE DECLARARON EN CADA UNO DE LOS ROUTING 
   path: '', loadChildren:()=>import('./modules/perfil/perfil.module').then(m=>m.PerfilModule)},{   //ruta de perfil
   path: '', loadChildren:()=>import('./modules/rutinas/rutinas.module').then(m=>m.RutinasModule)},{   //ruta de rutinas
@@ -16,13 +16,12 @@ const routes: Routes = [
   //path: 'crear', component: CrearrutinasComponent},{   //ruta de crearrutinas
   path: '', loadChildren:()=>import('./modules/concepinf/concepinf.module').then(m=>m.ConcepinfModule)},
   {
-    path: 'visitante', loadChildren: () => import('./modules/visitante/visitante.module').then(m => m.VisitanteModule) //ruta de visitante
+    path: 'visitante', loadChildren: () => import('./modules/visitante/visitante.module').then(m => m.VisitanteModule),canActivate:[RoleGuard], data: { expect: Roles.Visitante } //ruta de visitante
   }, 
-  {
-    path: 'admin', loadChildren: ()=> import('./modules/admin/admin.module').then(m => m.AdminModule), //ruta de admin
-  },{
+  {path: 'admin', loadChildren: ()=> import('./modules/admin/admin.module').then(m => m.AdminModule),canActivate:[RoleGuard], data: { expect: Roles.Admin }, //ruta de admin
+},{
   path: '', loadChildren:()=>import('./modules/concepinf/concepinf.module').then(m=>m.ConcepinfModule)},{ //ruta de concepinf
-  path: '', loadChildren:()=>import('./modules/calculadora-cal/calculadora-cal.module').then(m=>m.CalculadoraCALModule)}
+  path: '', loadChildren:()=>import('./modules/calculadora-cal/calculadora-cal.module').then(m=>m.CalculadoraCALModule)},
 ];
 
 @NgModule({
