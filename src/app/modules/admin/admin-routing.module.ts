822480f918ce36eec8f3ee48dd7ca01d8ffe86cf
Gauna1553 <gabrielgauna04@gmail.com>
2023-11-01 11:12:17 -0300

    cree la ruta para la vista de admin

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminModule } from './admin.module';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'admin', component: AdminModule}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
