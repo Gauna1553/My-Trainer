46b0c37452b7b0d5ca14752a74c037c70780a5ec
dariannfuentes <dariannfuentes@gmail.com>
2023-11-05 20:55:38 -0300

    Cambie el tipo de dato que podria tener idRutina en la interface Rutina

--- src/app/model/rutinas.ts
+++ src/app/model/rutinas.ts
@@ -2,7 +2,7 @@ import { Ejercicio } from "./ejercicios";
 
 // Estructura que tendran las rutinas
 export interface Rutina {
-    idRutina: string;
+    idRutina: string | any;
     nombre: string;
     ejercicios: Ejercicio[];
 }
