fbaa3940fde0d62a96dc8c36e4a2664b2f655dc8
Gauna1553 <gabrielgauna04@gmail.com>
2023-11-06 09:28:02 -0300

    borre importaciones del ejercicios module

--- src/app/modules/ejercicios/ejercicios.module.ts
+++ src/app/modules/ejercicios/ejercicios.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EjerciciosComponent } from './ejercicios.component';
-import { EjerciciosRoutingModule } from './ejercicios-routing.module';
 
 
 //Importaciones de Prime
@@ -12,8 +11,6 @@ import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { CheckboxModule } from 'primeng/checkbox';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -30,11 +27,8 @@ import { CheckboxModule } from 'primeng/checkbox';
     InputTextModule,
     ButtonModule,
     TableModule,
-    EjerciciosRoutingModule,
     TriStateCheckboxModule,
     ProgressSpinnerModule,
-    CheckboxModule,
-    //BrowserAnimationsModule
   ], 
   exports: [
     FormsModule,
