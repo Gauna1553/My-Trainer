b4f646f8f91f2d6170b8612b58637b901ca05c59
Gauna1553 <gabrielgauna04@gmail.com>
2023-11-07 10:25:57 -0300

    intente implementar una tabla pero no me salio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
-import { RoleGuard } from './modules/guard/roles.guard';
 import { VisitanteComponent } from './modules/visitante/visitante.component';
 
 
