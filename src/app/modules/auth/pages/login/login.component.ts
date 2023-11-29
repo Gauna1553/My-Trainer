3be8376d8749d910b98d146e936813bbf7a21b3b
dariannfuentes <dariannfuentes@gmail.com>
2023-11-29 17:46:19 -0300

    Me subscribi al observable para saber si esta logeado el usuario y les puse ngif a las etiquetas para separar cuales se van a ver

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -29,13 +29,29 @@ export class LoginComponent {
   constructor(
     public servicioAuth: AuthService,
     public firestore: FirestoreService,
-    public router :Router,
-    private afAuth: AngularFireAuth, 
+    public router: Router,
+    private afAuth: AngularFireAuth,
     private fireStore: AngularFirestore,
     private http: HttpClient,
     private cookieService: CookieService
     //Estas son las declaraciones de las importaciones de Firebase a para poder utilizar
-    ){}
+  ) { }
+  
+  //Aca lo declaramos falso como default para que por las dudas no muestre nada
+  loggedIn = false;
+
+  ngOnInit() {
+    //Aca lo que hacemos es que al iniciar el componente se subscriba al observable que nos dice si el usuario esta logeado o no
+    this.servicioAuth.isLoggedIn().subscribe(isLoggedIn => {
+      //Aca recibimos el valor en el parametro 'isLoggedIn' y comparamos si es verdadero o falso que esta logeado
+      if (isLoggedIn) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+    })
+  }
+
 
   usuarios: Usuario = {
     uid: '',
@@ -53,34 +69,34 @@ export class LoginComponent {
     //Arreglo en donde se guardarar en los parametros del objeto Ejercicio
   }
 
-  async iniciar(){
+  async iniciar() {
     const credenciales = {
-    email: this.usuarios.email,
-    contrasena: this.usuarios.contrasena
-    //guarda los parametros email y constraseña en una constante
+      email: this.usuarios.email,
+      contrasena: this.usuarios.contrasena
+      //guarda los parametros email y constraseña en una constante
     };
 
     const res = await this.servicioAuth.iniciarSesion(credenciales.email!, credenciales.contrasena!)
-    .then ((res) => {
-      ///////Base de datos///////
-      this.afAuth.authState.subscribe(usuario => {
-        if (usuario) {
-          this.fireStore.collection('usuarios').doc(usuario.uid).valueChanges().subscribe((data: any)=> {
-            //Aqui se obtienen las credenciales del usuario
-            this.servicioAuth.login(data.rol)
-            //Rediriges al usuario basado en sus credenciales
-            this.router.navigate(['/'])
-          })
-        } /*else {
+      .then((res) => {
+        ///////Base de datos///////
+        this.afAuth.authState.subscribe(usuario => {
+          if (usuario) {
+            this.fireStore.collection('usuarios').doc(usuario.uid).valueChanges().subscribe((data: any) => {
+              //Aqui se obtienen las credenciales del usuario
+              this.servicioAuth.login(data.rol)
+              //Rediriges al usuario basado en sus credenciales
+              this.router.navigate(['/'])
+            })
+          } /*else {
           //Usuario no logeado
           alert('No se ha podido ingresar')
           this.router.navigate(['/inicio']);
         }*/
-      })
-    }) .catch((error) => {
-      console.error(error)
-      //Usuario no valido
-    });
+        })
+      }).catch((error) => {
+        console.error(error)
+        //Usuario no valido
+      });
 
     /*
       Esta función se encarga de recorrer la BD en busca de los datos de email y contraseñas almacenadas para asi poder permitirle al usuario
