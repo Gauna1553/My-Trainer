e3dd5d2853b721f9fd200ddd91578589a554daa9
dariannfuentes <dariannfuentes@gmail.com>
2023-11-29 19:51:33 -0300

    Hice que la navbar muestre diferentes items segun si sos visitante, usuario o admin

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -10,6 +10,7 @@ import { AuthService } from 'src/app/modules/auth/services/auth.service';
 export class NavbarComponent implements OnInit {
   declare items: MenuItem[];
   stopExecution = true; //Esto evita que el ngOnInit se ejecute en bucle en caso de que el usuario sea admin
+  stopExecution2 = true; //Esto evita que el enOInit se ejecute en bucle en caso de que el usuario sea usuario
   //Aca lo declaramos falso como default para que por las dudas no muestre nada
   loggedIn = false;
   isAdmin: boolean = false;
@@ -19,23 +20,28 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() {
+    //Aca lo que hacemos es que al iniciar el componente se subscriba al observable que nos dice que tipo de usuario se logeo
+    this.servicioAuth.getUserType().subscribe(userType => {
+      if(userType === 'admin' && this.stopExecution){
+        this.stopExecution = false; //Al volverse falso nos bloquea el paso en el if ya que no cumple más la condición de true
+        this.isAdmin = true; //Esto marca que el usuario es administrador
+        this.ngOnInit();
+      }else{
+        if(userType === 'usuario' && this.stopExecution2){
+          this.stopExecution2 = false; //Al volverse falso nos bloque el paso en el if ya que no cumple más la condición de true
+          this.ngOnInit();
+        }
+      }
+    })
+
     //Aca lo que hacemos es que al iniciar el componente se subscriba al observable que nos dice si el usuario está logeado o no
     this.servicioAuth.isLoggedIn().subscribe(isLoggedIn => {
       //Aca recibimos el valor en el parametro 'isLoggedIn' y comparamos si es verdadero o falso que está logeado
       if (isLoggedIn) {
         this.loggedIn = true;
-      } else {
-        this.loggedIn = false;
       }
     })
-    //Aca lo que hacemos es que al iniciar el componente se subscriba al observable que nos dice que tipo de usuario se logeo
-    this.servicioAuth.getUserType().subscribe(userType => {
-      if(userType === 'admin' && this.stopExecution){
-        this.stopExecution = false; //Al volverse falso nos bloquea el paso en el if ya que no cumple más la condición de true
-        this.isAdmin = true;
-        this.ngOnInit();
-      }
-    })
+
   //Items de la navbar y sus propiedades
   this.items = [
     {
