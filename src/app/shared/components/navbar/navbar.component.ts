f7dfc7a55549fe4f0950fc4ecf6fa64f5d7a9a07
dariannfuentes <dariannfuentes@gmail.com>
2023-11-29 18:37:49 -0300

    Cambie la navbar para que solo muestre la seccion de rutinas y ejercicios si el usuario se logea

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -10,17 +10,28 @@ import { AuthService } from 'src/app/modules/auth/services/auth.service';
 })
 export class NavbarComponent implements OnInit {
   declare items: MenuItem[];
+  stopExecution = true; //Esto evita que el ngOnInit se ejecute en bucle en caso de que el usuario sea admin
+  //Aca lo declaramos falso como default para que por las dudas no muestre nada
+  loggedIn = false;
   isAdmin: boolean = false;
-  stopExecution = true
 
   constructor(private servicioAuth: AuthService){
     
   }
 
   ngOnInit() {
+    //Aca lo que hacemos es que al iniciar el componente se subscriba al observable que nos dice si el usuario está logeado o no
+    this.servicioAuth.isLoggedIn().subscribe(isLoggedIn => {
+      //Aca recibimos el valor en el parametro 'isLoggedIn' y comparamos si es verdadero o falso que está logeado
+      if (isLoggedIn) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+    })
     this.servicioAuth.getUserType().subscribe(userType => {
       if(userType === 'admin' && this.stopExecution){
-        this.stopExecution = false
+        this.stopExecution = false; //Al volverse falso nos bloquea el paso en el if ya que no cumple más la condición de true
         this.isAdmin = true;
         this.ngOnInit();
       }
@@ -35,11 +46,13 @@ export class NavbarComponent implements OnInit {
       label: 'Ejercicios',
       icon: 'pi pi-fw pi-pencil',
       routerLink: '/ejercicios',
+      visible: this.loggedIn
     },
     {
       label: 'Rutinas',
       icon: 'pi pi-fw pi-calendar',
       routerLink: '/rutinas',
+      visible: this.loggedIn
     },
     {
       label: 'Perfil',
@@ -47,6 +60,7 @@ export class NavbarComponent implements OnInit {
       routerLink: '/perfil',
     },{
       label: 'Administrador',
+      icon: 'pi pi-id-card',
       routerLink: '/admin',
       visible: this.isAdmin
     }
