99402524911daa6940e0596f880ad3c8348050df
dariannfuentes <dariannfuentes@gmail.com>
2023-11-28 21:06:32 -0300

    Me subscribi al observable para saber cuando un usuario se loggea y si es admin y en base a eso mostrar en la navbar la seccion de administrador

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Usuario } from 'src/app/model/usuarios';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,31 +10,43 @@ import { Usuario } from 'src/app/model/usuarios';
 })
 export class NavbarComponent implements OnInit {
   declare items: MenuItem[];
+  isAdmin: boolean = false
 
-
+  constructor(private servicioAuth: AuthService){
+    this.servicioAuth.getUserType().subscribe(userType => {
+      if(userType === 'admin'){
+        this.isAdmin = true;
+        this.ngOnInit();
+      }
+    })
+  }
 
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Inicio',
-        icon: 'pi pi-home',
-        routerLink: '/',
-      },
-      {
-        label: 'Ejercicios',
-        icon: 'pi pi-fw pi-pencil',
-        routerLink: '/ejercicios',
-      },
-      {
-        label: 'Rutinas',
-        icon: 'pi pi-fw pi-calendar',
-        routerLink: '/rutinas',
-      },
-      {
-        label: 'Perfil',
-        icon: 'pi pi-user',
-        routerLink: '/perfil',
-      },
-    ];
-  }
-};
+  this.items = [
+    {
+      label: 'Inicio',
+      icon: 'pi pi-home',
+      routerLink: '/',
+    },
+    {
+      label: 'Ejercicios',
+      icon: 'pi pi-fw pi-pencil',
+      routerLink: '/ejercicios',
+    },
+    {
+      label: 'Rutinas',
+      icon: 'pi pi-fw pi-calendar',
+      routerLink: '/rutinas',
+    },
+    {
+      label: 'Perfil',
+      icon: 'pi pi-user',
+      routerLink: '/perfil',
+    },{
+      label: 'Administrador',
+      routerLink: '/admin',
+      visible: this.isAdmin
+    }
+  ];}
+  
+}
